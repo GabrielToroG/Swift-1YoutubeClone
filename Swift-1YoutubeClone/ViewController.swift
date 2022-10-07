dd967c65148e8207fa1ffe98a6181c4ff4c55adb
Gtoro <gabrielt@soymomo.com>
2022-10-07 13:53:05 -0300

    Initial Commit

---
+++ Swift-1YoutubeClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift-1YoutubeClone
+//
+//  Created by Gabriel Toro on 07-10-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
